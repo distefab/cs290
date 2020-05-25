e63dd859e60a8a8225c20f275aecb466fc4916cd
distefab <distefab@oregonstate.edu>
2020-05-24 23:02:45 -0700

    testing again

--- ucWebsite/home.js
+++ ucWebsite/home.js
@@ -7,13 +7,12 @@ var handlebars = require('express-handlebars').create({defaultLayout:'page'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
-
+app.use(express.static(__dirname + '/public'));
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(("/css")));
 
 app.get('/', function(req,res){
     
