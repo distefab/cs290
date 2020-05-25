aca36df129ca45397b7b6f735c90089abe244890
distefab <distefab@oregonstate.edu>
2020-05-25 03:57:43 -0700

    g

--- ucWebsite/home.js
+++ ucWebsite/home.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get('/', function(req,res, next){
+app.get('/', function(req,res){
     
     res.render('navbar');
   });
