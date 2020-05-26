59fb473b37e0afd2d84678b34dc4fba80311cbd3
distefab <distefab@oregonstate.edu>
2020-05-25 22:32:01 -0700

    testing

--- ucWebsite/home.js
+++ ucWebsite/home.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mysql = require('./dbcon.js');
 
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'nojs'});
+var handlebars = require('express-handlebars').create({defaultLayout:'nojs',layoutsDir:"views/layouts"});
 
 
 app.engine('handlebars', handlebars.engine);
@@ -17,33 +17,33 @@ app.use(bodyParser.json());
 
 app.get('/', function(req,res){
     
-    res.render('navbar_home');
+    res.render('navbar_home', {layout:'page'});
   });
 
   app.get('/menu', function(req,res){
     
-    res.render('navbar_menu', {Layout:'nojs'});
+    res.render('navbar_menu');
   });
 
   app.get('/contact', function(req,res){
     
-    res.render('navbar_contact',{Layout:'nojs'});
+    res.render('navbar_contact');
   });
 
   app.get('/about', function(req,res){
     
-    res.render('navbar_about',{Layout:'nojs'});
+    res.render('navbar_about');
   });
 
 app.use(function(req,res){
   res.status(404);
-  res.render('404',{Layout:'nojs'});
+  res.render('404');
 });
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
-  res.render('500',{Layout:'nojs'});
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
