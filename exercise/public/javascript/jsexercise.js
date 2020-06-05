b018bdc1e134a48b641b90b02ac2257fdac32b8e
distefab <distefab@oregonstate.edu>
2020-06-05 11:11:48 -0700

    test

--- exercise/public/javascript/jsexercise.js
+++ exercise/public/javascript/jsexercise.js
@@ -1,39 +1,39 @@
 var elements = document.getElementsByClassName("edit");
 Array.from(elements).forEach(function(element) {
     element.addEventListener('click', function(event){
-        var myId = event.target.getAttribute('rowid');
-        console.log(myId);
-        var editButton = document.getElementById(myId + '_editButton');
-        var submitButton = document.getElementById(myId + '_submitButton');
-        var name = document.getElementById(myId + '_name');
-        var reps = document.getElementById(myId+ '_reps');
-        var weight = document.getElementById(myId + '_weight');
-        var date = document.getElementById(myId+'_date');
-        var pounds = document.getElementById(myId + '_lbs');
-        var kilos = document.getElementById(myId + '_kg');
+        var exerciseid = event.target.getAttribute('rowid');
+        console.log(exerciseid);
+        var editButton = document.getElementById(exerciseid + '_editButton');
+        var submitButton = document.getElementById(exerciseid + '_submitButton');
+        var name = document.getElementById(exerciseid + '_name');
+        var reps = document.getElementById(exerciseid+ '_reps');
+        var weight = document.getElementById(exerciseid + '_weight');
+        var date = document.getElementById(exerciseid+'_date');
+        var pounds = document.getElementById(exerciseid + '_lbs');
+        var kilos = document.getElementById(exerciseid + '_kg');
         editButton.style.display = 'none';
-        submitButton.style.display ='inline';
-        name.style.display = 'inline';
-        reps.style.display = 'inline';
-        weight.style.display = 'inline';
-        date.style.display = 'inline';
-        pounds.style.display = 'inline';
-        kilos.style.display = 'inline';
+        submitButton.style.display = '';
+        name.style.display = '';
+        reps.style.display = '';
+        weight.style.display = '';
+        date.style.display = '';
+        pounds.style.display = '';
+        kilos.style.display = '';
         var req = new XMLHttpRequest();  
-        submitForm(myId);
+        submitForm(exerciseid);
             
     }
 )});
 function myFunction(event){
-    var myId = event.currentTarget.rowId;
-    var editButton = document.getElementById(myId + '_editButton');
-    var submitButton = document.getElementById(myId + '_submitButton');
-    var name = document.getElementById(myId + '_name');
-    var reps = document.getElementById(myId+ '_reps');
-    var weight = document.getElementById(myId + '_weight');
-    var date = document.getElementById(myId+'_date');
-    var pounds = document.getElementById(myId + '_lbs');
-    var kilos = document.getElementById(myId + '_kg');
+    var exerciseid = event.currentTarget.rowId;
+    var editButton = document.getElementById(exerciseid + '_editButton');
+    var submitButton = document.getElementById(exerciseid + '_submitButton');
+    var name = document.getElementById(exerciseid + '_name');
+    var reps = document.getElementById(exerciseid+ '_reps');
+    var weight = document.getElementById(exerciseid + '_weight');
+    var date = document.getElementById(exerciseid+'_date');
+    var pounds = document.getElementById(exerciseid + '_lbs');
+    var kilos = document.getElementById(exerciseid + '_kg');
     editButton.style.display = 'block';
     submitButton.style.display = 'block';
     name.style.display = 'block';
@@ -42,37 +42,37 @@ function myFunction(event){
     date.style.display = 'block';
     pounds.style.display = 'block';
     kilos.style.display = 'block';
-    console.log(myId);
+    console.log(exerciseid);
     // var req = new XMLHttpRequest();  
-    submitForm(myId);
+    submitForm(exerciseid);
     
 };
 
-function editRow(myId, myLbs){  
+function editRow(exerciseid, myLbs){  
     //Hide Edit Button
-    var editButt = document.getElementById(myId + '_editButton');
+    var editButt = document.getElementById(exerciseid + '_editButton');
     editButt.style.display = 'none';
 
 
-    var submitButt = document.getElementById(myId + '_submitButton');
+    var submitButt = document.getElementById(exerciseid + '_submitButton');
     submitButt.style.display = 'block';
 
-    var thisName = document.getElementById(myId + '_name');
+    var thisName = document.getElementById(exerciseid + '_name');
     thisName.style.display = 'block';    
 
-    var thisReps = document.getElementById(myId+ '_reps');
+    var thisReps = document.getElementById(exerciseid+ '_reps');
     thisReps.style.display = 'block';
 
-    var thisWeight = document.getElementById(myId + '_weight');
+    var thisWeight = document.getElementById(exerciseid + '_weight');
     thisWeight.style.display = 'block';
 
-    var thisDate = document.getElementById(myId+'_date');
+    var thisDate = document.getElementById(exerciseid+'_date');
     thisDate.style.display = 'block';
 
-    var thisLbs = document.getElementById(myId + '_lbs');
+    var thisLbs = document.getElementById(exerciseid + '_lbs');
     thisLbs.style.display = 'block';
 
-    var thisKg = document.getElementById(myId + '_kg');
+    var thisKg = document.getElementById(exerciseid + '_kg');
     thisKg.style.display = 'block';
       
     submitForm();
@@ -80,63 +80,63 @@ function editRow(myId, myLbs){
 }
 
 
- function submitForm(myId){
-        var thisId = document.getElementById(myId);
-        var editButt = document.getElementById(myId + '_editButton');
+ function submitForm(exerciseid){
+        var thisId = document.getElementById(exerciseid);
+        var editButt = document.getElementById(exerciseid + '_editButton');
 
-        var editButt = document.getElementById(myId + '_editButton');
+        var editButt = document.getElementById(exerciseid + '_editButton');
         editButt.style.display = 'block';
 
-        var submitButt = document.getElementById(myId + '_submitButton');
+        var submitButt = document.getElementById(exerciseid + '_submitButton');
         submitButt.style.display = 'none';
 
-        var thisName = document.getElementById(myId + '_name');
+        var thisName = document.getElementById(exerciseid + '_name');
         thisName.style.display = 'none';    
 
-        var thisReps = document.getElementById(myId+ '_reps');
+        var thisReps = document.getElementById(exerciseid+ '_reps');
         thisReps.style.display = 'none';
 
-        var thisWeight = document.getElementById(myId + '_weight');
+        var thisWeight = document.getElementById(exerciseid + '_weight');
         thisWeight.style.display = 'none';
 
-        var thisDate = document.getElementById(myId+'_date');
+        var thisDate = document.getElementById(exerciseid+'_date');
         thisDate.style.display = 'none';
 
-        var thisLbs = document.getElementById(myId + '_lbs');
+        var thisLbs = document.getElementById(exerciseid + '_lbs');
         thisLbs.style.display = 'none';
 
-        var thisKg = document.getElementById(myId + '_kg');
+        var thisKg = document.getElementById(exerciseid + '_kg');
         thisKg.style.display = 'none';
 
 
   
     
  }
- function deleteRow(myId){
-    var thisId = document.getElementById(myId);
+ function deleteRow(exerciseid){
+    var thisId = document.getElementById(exerciseid);
 
-    var editButt = document.getElementById(myId + '_editButton');
+    var editButt = document.getElementById(exerciseid + '_editButton');
     editButt.style.display = 'none';
 
-    var submitButt = document.getElementById(myId + '_submitButton');
+    var submitButt = document.getElementById(exerciseid + '_submitButton');
     submitButt.style.display = 'none';
 
-    var thisName = document.getElementById(myId + '_name');
+    var thisName = document.getElementById(exerciseid + '_name');
     thisName.style.display = 'none';    
 
-    var thisReps = document.getElementById(myId+ '_reps');
+    var thisReps = document.getElementById(exerciseid+ '_reps');
     thisReps.style.display = 'none';
 
-    var thisWeight = document.getElementById(myId + '_weight');
+    var thisWeight = document.getElementById(exerciseid + '_weight');
     thisWeight.style.display = 'none';
 
-    var thisDate = document.getElementById(myId+'_date');
+    var thisDate = document.getElementById(exerciseid+'_date');
     thisDate.style.display = 'none';
 
-    var thisLbs = document.getElementById(myId + '_lbs');
+    var thisLbs = document.getElementById(exerciseid + '_lbs');
     thisLbs.style.display = 'none';
 
-    var thisKg = document.getElementById(myId + '_kg');
+    var thisKg = document.getElementById(exerciseid + '_kg');
     thisKg.style.display = 'none';
 
 
