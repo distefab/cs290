8444223e367d7d038598e05cd63a6051b69f1cbf
distefab <distefab@oregonstate.edu>
2020-06-04 22:52:44 -0700

    changed rep

--- exercise/home.js
+++ exercise/home.js
@@ -55,7 +55,7 @@ app.get('/update',function(req,res,next){
     if(result.length == 1){
       var curVals = result[0];
       mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
-        [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, rep.query.date || curVals.date, rep.query.lbs || curVals.lbs,  req.query.id],
+        [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.date || curVals.date, req.query.lbs || curVals.lbs,  req.query.id],
         function(err, result){
         if(err){
           next(err);
